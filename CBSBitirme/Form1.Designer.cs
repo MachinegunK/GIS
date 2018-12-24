555d2ac9c67118d9a5dc501407bd47137f14b24c
MachinegunK <machinegunkings@gmail.com>
2018-12-25 01:09:44 +0300

    Add files via upload

---
+++ CBSBitirme/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace CBSBitirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Location = new System.Drawing.Point(276, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 536);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 555);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear Map";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "15",
+            "16"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 121);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 28);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "Select year of choice";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(45, 434);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 31);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "SHOW";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SHOW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 30);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "SHOW";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total emission\r\n";
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Location = new System.Drawing.Point(989, 36);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(322, 536);
+            this.panel4.TabIndex = 6;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            this.panel4.Resize += new System.EventHandler(this.Form1_Resize);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Otomobil",
+            "Minibüs",
+            "Otobüs",
+            "Kamyonet",
+            "Kamyon",
+            "Motosiklet",
+            "ÖzelAmaç",
+            "Yolİş",
+            "Traktör"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 155);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(245, 28);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.Text = "Select type of vehicle";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Automobile Distribution";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Factories and Forest Cover";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Factories",
+            "Forest Cover",
+            "Both"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 263);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(245, 28);
+            this.comboBox3.TabIndex = 15;
+            this.comboBox3.Text = "Select one";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(45, 374);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(100, 24);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Factories";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(45, 404);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 24);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Automobile";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 494);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(265, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Air Quality with respect to vegetation\r\n";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(45, 517);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 32);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "DISPLAY";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = " type here";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Enter Range Value";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 591);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AIR QUALITY ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CBSBitirme/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MapInfo;
+using System.Runtime.InteropServices;
+
+namespace CBSBitirme
+{
+    public partial class Form1 : Form
+    {//global variables here
+        public static MapInfo.MapInfoApplication mi;
+        public static string win_id;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            mi = new MapInfo.MapInfoApplication();
+            mi = Activator.CreateInstance(Type.GetTypeFromProgID("Mapinfo.Application")) as MapInfoApplication;
+            int p = panel2.Handle.ToInt32();
+            
+
+            mi.Do("set next document parent " + p.ToString() + "style 1");
+            mi.Do("set application window " + p.ToString());
+            mi.Do("run application \"" + "D:/GIS3Workspace/deneme.wor" + "\"");
+            win_id = mi.Eval("frontwindow()");
+
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (mi != null)
+            {
+                // The form has been resized. 
+                if (mi.Eval("WindowID(0)") != "")
+                {
+                    // Update the map to match the current size of the panel. 
+                    MoveWindow((System.IntPtr)long.Parse(mi.Eval("WindowInfo(FrontWindow(),12)")), 0, 0, this.panel2.Width, this.panel2.Height, false);
+                    
+                }
+
+            }
+
+        }
+            private void button1_Click(object sender, EventArgs e)
+        {
+            removetematik();
+            string p = panel2.Handle.ToString();
+            string p4 = panel4.Handle.ToString();
+            int n = Convert.ToInt16(textBox1.Text);
+            string thematic_column = string.Empty;
+            string arac_yili = Convert.ToString(comboBox1.SelectedItem);
+            string arac_tipi = Convert.ToString(comboBox2.SelectedItem);
+            
+            string sorgu1 = arac_tipi + arac_yili;
+
+           
+
+            try
+            {
+                mi.Do("Add Column \"Iller\" (" + sorgu1 + " Integer)From data1 Set To " + sorgu1 + " Where COL2 = COL1  Dynamic");
+            }
+            catch
+            {
+            }
+            thematic_column = sorgu1;
+            mi.Do("select " + sorgu1 + " from Iller order by " + sorgu1 + " into sel");
+
+
+
+            //tematik oluşturma
+            int range = Convert.ToInt16(mi.Eval("int(tableinfo(sel,8)/" + Convert.ToString(n) + ")"));
+            int c_range = Convert.ToInt16(255 / n);
+            //----------part 2 -----
+            mi.Do("fetch first from sel");
+            string r1 = Convert.ToString(Form1.mi.Eval("sel.col1"));
+            string r2 = string.Empty;
+            string cmstr = string.Empty;
+
+            for (int i = 1; i < n; i++)
+            {
+                mi.Do("fetch rec " + Convert.ToString(i * range) + " from sel");
+                r2 = Convert.ToString(mi.Eval("sel.col2"));
+                string rgb = Convert.ToString(mi.Eval("RGB(255," + Convert.ToString((n - i) * c_range) + "," + Convert.ToString((n - i) * c_range) + ")"));
+                cmstr = cmstr + r1 + ":" + r2 + " brush(2," + rgb + ",16777215), ";
+                r1 = r2;
+            }
+            mi.Do("fetch last from sel");
+            r2 = Convert.ToString(mi.Eval("sel.col1"));
+            cmstr = cmstr + r1 + ":" + r2 + " brush(2,16711680,16777215)";
+            // ----------part 3 -----
+            mi.Do("shade window " +Form1.win_id + " Iller with " + thematic_column + " ranges apply all use color Brush (2,16711680,16777215) " + cmstr);
+            mi.Do("Set Next Document Parent " + p4 + " Style 1");
+            mi.Do("Create Cartographic Legend From Window " + win_id + " Behind Frame From Layer 1");
+            Form1.mi.Do("select * from iller where plaka_no =\"82\" into sel");
+            Form1.mi.Do("fetch last from sel");
+            r2 = Convert.ToString(Form1.mi.Eval("sel.col1"));
+            cmstr = cmstr + r1 + ":" + r2 + " brush(2,255,16777215)";
+            //kod sonu
+
+
+            
+
+
+
+
+        }
+
+
+        //TEMATİK SİLME
+
+        public void removetematik()
+        {
+            for (int k = Convert.ToInt16(Form1.mi.Eval("mapperinfo(" + Form1.win_id + ",9)")); k > 0; k = k - 1)
+            {
+                if (Convert.ToInt16(Form1.mi.Eval("layerinfo(" + Form1.win_id + "," + Convert.ToString(k) + ",24)")) == 3)
+                {
+                    Form1.mi.Do("remove map layer \"" + Form1.mi.Eval("layerinfo(" + Form1.win_id + "," + Convert.ToString(k) + ",1)") + "\"");
+                }
+            }
+
+        }
+
+   
+
+
+
+
+
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            removetematik();
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
